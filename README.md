faf004a62c3df050c36197302debe4e734d975f5
sammiefrog <sammiefrog@live.com>
2020-03-13 15:51:45 -0500

    made api call, started formatting

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Live Link
+https://sammiefrog.github.io/weather-dashboard/
+
+## Description
+This is a quiz made with javascript for functionality, with questions about me to test how well people know me. You earn points when you get questions correct, and you also earn more time. When you answer incorrectly you lose ten seconds. At the end you can save your name and score.
+
+## Technology Stack
+HTML, Bootstrap, JQuery
+
+## Contributors
+@sammiefrog
+
+## Usage
+To test your knowledge on me as a person.
+
+## Credits
+www.w3schools.com 
+
+
+## Contact
+* #### Name: Sammantha Sasenick (@sammiefrog)
+* #### Email: [sammiefrog@live.com](sammiefrog@live.com)
+* #### LinkedIn: www.linkedin.com/in/sammantha-sasenick412
+
+## License
+MIT
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,45 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+//moment
+var momentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(momentTime);
+
+//variables
+var APIKey = 'c713cdaa927c21aadd4497732b1e2f54';
+var cityInput = $('#cityInput').val().trim();
+var submitCity = $('#submit-city');
+var cityHistory = $('city-history');
+var currentWeather = $('#current-weather');
+var fiveDay = $('#five-day');
+
+
+function getCurrentWeather() {
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q="+ cityInput + "&appid=" + APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(weatherData) {
+        console.log(weatherData);
+        console.log(queryURL);
+
+    });
+
+}
+
+submitCity.on('click', function(event) {
+    event.preventDefault();
+
+    alert('clicking');
 
 
 
-
+    getCurrentWeather();
+});
 
 //delete this and you break
 });
