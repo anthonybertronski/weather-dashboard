9b49c5b2b9a08f145781d91d848e1e11daafe3b7
sammiefrog <sammiefrog@live.com>
2020-03-16 12:14:51 -0500

    prepending city under search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+console.log( "ready!" );
 
 //moment
 var momentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -11,12 +10,12 @@ var submitCity = $('#submit-city');
 var cityHistory = $('#city-history');
 var currentWeather = $('#current-weather');
 var fiveDay = $('#five-day');
-
+var keepHistory = JSON.parse(localStorage.getItem("searchHistory") || [] );
 
 
 function getCurrentWeather() {
-    
     var cityInput = $('#cityInput').val().trim();
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityInput + "&appid=" + APIKey;
 
   // Here we run our AJAX call to the OpenWeatherMap API
@@ -30,19 +29,37 @@ $.ajax({
         console.log(weatherData);
         console.log(queryURL);
 
+        getMoreWeather(cityInput, queryURL);
+
     });
+
+}
+
+function getMoreWeather (city, url) {
+    console.log(city);
+    console.log(url);
 
 }
 
 submitCity.on('click', function(event) {
     event.preventDefault();
+    var cityHistDiv = $('<div>');
+    var cityInput = $('#cityInput').val().trim();
 
-    alert('clicking');
+    cityHistDiv.text(cityInput);
+    cityHistDiv.prependTo(cityHistory);
+
+
+    // keepHistory.push(cityInput);
+
+    localStorage.setItem("searchHistory", JSON.stringify(keepHistory));
+
     console.log(cityInput);
 
 
     getCurrentWeather();
 });
 
+$( document ).ready(function() {
 //delete this and you break
 });
